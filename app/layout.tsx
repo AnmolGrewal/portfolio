a731d4a027a72b52c8d443a1a4ab62b83edc90bc
anmolgrewal <anmolsgrewal@gmail.com>
2024-08-13 07:22:30 -0700

    Updating Copyright and page header

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Anmol | Personal Portfolio",
+  description:
+    "Anmol is a full-stack developer with over 4 years of experience.",
 };
 
 export default function RootLayout({
