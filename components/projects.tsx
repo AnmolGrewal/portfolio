b9a2604099cfb9e0be87af7e93a17ed9f355cbaf
anmolgrewal <anmolsgrewal@gmail.com>
2024-08-13 18:51:21 -0700

    Updating Projects to support more text variable based

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import React from "react";
-import SectionHeading from "./section-heading";
-import { projectsData } from "@/lib/data";
-import Project from "./project";
-import { useSectionInView } from "@/lib/hooks";
+import React from 'react';
+import SectionHeading from './section-heading';
+import { projectsData } from '@/lib/data';
+import Project from './project';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
+  const { ref } = useSectionInView('Projects', 0.5);
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
-      <div>
+      <div className="flex flex-col gap-8">
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
--- lib/data.ts
+++ lib/data.ts
@@ -1,34 +1,33 @@
-import React from "react";
-import { FaReact, FaSalesforce, FaTiktok } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import React from 'react';
+import { FaReact, FaSalesforce, FaTiktok } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import speedyNexus from '@/public/speedynexus.png';
+import lostMokokos from '@/public/lostmokokos.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
 ] as const;
 
@@ -42,35 +41,34 @@ type Experience = {
 
 export const experiencesData: readonly Experience[] = [
   {
-    title: "Bachelor of Science - Computer Science",
-    location: "Simon Fraser University",
-    description:
-      "Graduated with a BS in Computer Science. Active member of the Computing Science Student Society.",
+    title: 'Bachelor of Science - Computer Science',
+    location: 'Simon Fraser University',
+    description: 'Graduated with a BS in Computer Science. Active member of the Computing Science Student Society.',
     icon: React.createElement(LuGraduationCap),
-    date: "2017 - 2020",
+    date: '2017 - 2020',
   },
   {
-    title: "Software Engineer Intern",
-    location: "Salesforce",
+    title: 'Software Engineer Intern',
+    location: 'Salesforce',
     description:
-      "Worked on various company and nonprofit codebases, created RESTful API integrations, implemented custom solutions, and collaborated in an Agile environment. Contributed to projects totaling over $100,000 in revenue.",
+      'Worked on various company and nonprofit codebases, created RESTful API integrations, implemented custom solutions, and collaborated in an Agile environment. Contributed to projects totaling over $100,000 in revenue.',
     icon: React.createElement(FaSalesforce),
-    date: "Jan 2019 - Aug 2019",
+    date: 'Jan 2019 - Aug 2019',
   },
   {
-    title: "Software Engineer",
-    location: "Harris Computer",
+    title: 'Software Engineer',
+    location: 'Harris Computer',
     description:
-      "Replaced legacy systems with React components, implemented Golang backend with GraphQL, authored best practices documentation, and enhanced application performance. Actively participated in design meetings and stakeholder discussions.",
+      'Replaced legacy systems with React components, implemented Golang backend with GraphQL, authored best practices documentation, and enhanced application performance. Actively participated in design meetings and stakeholder discussions.',
     icon: React.createElement(FaReact),
-    date: "Mar 2021 - Aug 2024",
+    date: 'Mar 2021 - Aug 2024',
   },
   {
-    title: "Software Engineer",
-    location: "TikTok",
-    description: "Inspiring creativity and bringing joy",
+    title: 'Software Engineer',
+    location: 'TikTok',
+    description: 'Inspiring creativity and bringing joy',
     icon: React.createElement(FaTiktok),
-    date: "September 2024 - Present",
+    date: 'September 2024 - Present',
   },
 ];
 
@@ -78,46 +76,39 @@ export const reversedExperiencesData = [...experiencesData].reverse();
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: 'NEXUS Interview Alert System',
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      'Displays currently available or newly released dates for NEXUS Interviews. Reducing wait times by up to ~90% by allowing users to secure cancelled or postponed interviews quickly.',
+    tags: ['React', 'Next.js', 'Tailwind', 'MUI', 'JSON'],
+    imageUrl: speedyNexus,
   },
   {
-    title: "rmtDev",
+    title: 'Amazon Game Studios – Lost Ark Community Website',
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      'Created a community website for Amazon Games published game Lost Ark, over 1 million page views and 10,000 users monthly. Utilizing React, Next.js and front-end libraries such as MUI, Fontawesome Icons to create tools for users to enhance their journey from scratch supporting Mobile and Desktop Users.',
+    tags: ['React', 'Next.js', 'Tailwind', 'MUI', 'Single Page Application'],
+    imageUrl: lostMokokos,
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Java",
-  "Python",
-  "Data Structures and Algorithms",
-  "Git",
-  "Tailwind",
-  "GraphQL",
-  "Leetcode",
-  "Artificial Intelligence",
-  "REST APIs",
-  "Object-Oriented Programming",
-  "Software Development Life Cycle",
-  "MySQL",
-  "Salesforce",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Java',
+  'Python',
+  'Data Structures and Algorithms',
+  'Git',
+  'Tailwind',
+  'GraphQL',
+  'Leetcode',
+  'Artificial Intelligence',
+  'REST APIs',
+  'Object-Oriented Programming',
+  'Software Development Life Cycle',
+  'MySQL',
+  'Salesforce',
 ] as const;
