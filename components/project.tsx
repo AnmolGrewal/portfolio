c035b2ac58598fe91f0ece9028f8e69e2ee377d1
anmolgrewal <anmolsgrewal@gmail.com>
2024-08-18 19:15:07 -0700

    Updating spacing between tags

--- components/project.tsx
+++ components/project.tsx
@@ -41,8 +41,8 @@ export default function Project({ title, description, tags, imageUrl, link, sour
         <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20 cursor-pointer">
           <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col h-full sm:group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{title}</h3>
-            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
-            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 mb-3">{description}</p>
+            <ul className="flex flex-wrap gap-2 sm:mt-auto">
               {tags.map((tag, index) => (
                 <li
                   className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -107,7 +107,7 @@ export default function Project({ title, description, tags, imageUrl, link, sour
                     href={link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-gray-500 hover:text-gray-700 text-2xl flex items-center"
+                    className="text-2xl flex items-center"
                     onClick={(e) => e.stopPropagation()}
                   >
                     <TbWorldWww size={24} className="mr-2" />
@@ -120,7 +120,7 @@ export default function Project({ title, description, tags, imageUrl, link, sour
                     href={sourceCodeLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-gray-500 hover:text-gray-700 text-2xl flex items-center"
+                    className="text-2xl flex items-center"
                     onClick={(e) => e.stopPropagation()}
                   >
                     <FaGithub size={24} className="mr-2" />
